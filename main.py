741a28a0d1adc87cee1dc6ce61724c829ed2f970
ns3838 <72807027+ns3838@users.noreply.github.com>
2020-10-20 11:10:36 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+light.set_pixel_color(0, light.rgb(255, 0, 0))
+light.set_pixel_color(1, light.rgb(0, 0, 255))
+light.set_pixel_color(2, light.rgb(0, 255, 0))
+light.set_pixel_color(3, light.rgb(255, 255, 0))
+light.set_pixel_color(4, light.rgb(255, 0, 255))
+light.set_pixel_color(5, light.rgb(0, 255, 255))
 
